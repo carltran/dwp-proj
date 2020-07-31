6e738cff8443db5d0180eaad7792e56999abd669
Carl Tran <carl.tran@valtech.com>
2020-07-31 19:48:43 +0100

    Add initial files

---
+++ src/main/java/com/carltran/dwpproj/DwpProjApplication.java
@@ -0,0 +1,13 @@
+package com.carltran.dwpproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwpProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carltran/dwpproj/DwpProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carltran.dwpproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwpProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
